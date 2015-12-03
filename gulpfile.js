c76e7fc7329b300c585d86509e3487c1ddc60b09
Hiroyuki Tanjo <expheno@gmail.com>
2015-12-03 12:36:41 +0900

    Example code and tasks

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+
+gulp.task('clean', del.bind(null, ['dist']));
+
+gulp.task('html', ['clean'], function () {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', ['html'], function () {
+  return gulp.src('src/main.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.postcss([], {to: 'dist/main.css'}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css:without-option', ['html'], function () {
+  return gulp.src('src/main.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.postcss([]))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css:sass-only', ['html'], function () {
+  return gulp.src('src/main.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
